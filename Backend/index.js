eca27e6fdff37df23fcc6dc53c445d36ae43774d
manishrajora25 <manishrajora453@gmail.com>
2025-10-03 14:44:35 +0530

    xdfgvhbjnk

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ import swaggerSpec from "./server/server.js"; // small s
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const allowedOrigins = [
   process.env.DEPLOYED_FRONTEND_URL,
