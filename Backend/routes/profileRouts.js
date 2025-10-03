baf968eb8298241c901dc9b2a3dc22eae8ece3b1
manishrajora25 <manishrajora453@gmail.com>
2025-10-03 15:01:40 +0530

    chdsjcc

--- Backend/routes/profileRouts.js
+++ Backend/routes/profileRouts.js
@@ -1,23 +1,4 @@
 
-// import express from "express";
-// import multer from "multer";
-// import { createOrUpdateProfile, getProfiles } from "../controllers/profileController.js";
-// import  checkToken  from "../middleware/checkToken.js";
-//  // JWT middleware
-
-// // Multer middleware (Cloudinary config already hogi)
-// const upload = multer({ storage: multer.memoryStorage() });
-
-// const router = express.Router();
-
-// router.post("/create", checkToken, upload.single("image"), createOrUpdateProfile);
-// router.get("/all", getProfiles);
-
-// export default router;
-
-
-
-
 
 import express from "express";
 import multer from "multer";
